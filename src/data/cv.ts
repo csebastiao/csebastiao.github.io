0932289480d3e8bf2ba93a679c1d093cc673048b
Clément Sebastiao <61236142+csebastiao@users.noreply.github.com>
2025-03-16 09:16:20 +0100

    Update cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,101 +1,95 @@
-export const cv = {
-  education: [
-    {
-      degree: "ENS Lyon Diploma",
-      school: "Ecole Normale Supérieure de Lyon",
-      location: "Lyon, France",
-      time: "2019-2023",
-    },
-    {
-      degree: "Master degree in Complex Systems, Physics",
-      school: "Ecole Normale Supérieure de Lyon",
-      location: "Lyon, France",
-      time: "2020-2022",
-    },
-    {
-      degree: "Bachelor degree in Physics",
-      school: "Claude Bernard University Lyon 1",
-      location: "Lyon, France",
-      time: "2017-2020",
-    },
-  ],
-  experience: [
-    {
-      title: "PhD Student",
-      company: "IT University of Copenhagen",
-      location: "Copenhagen, Denmark",
-      description: "Optimizing urban sustainable mobility networks, part of the Just Streets EU Horizon Program. Supervised by Michael Szell and Luca Maria Aiello.",
-      year: "April 2024-Present",
-    },
-    {
-      title: "Research Assistant",
-      company: "IT University of Copenhagen",
-      location: "Copenhagen, Denmark",
-      year: "January 2024-April 2024",
-    },
-    {
-      title: "Intern",
-      company: "Université de Franche-Comté",
-      location: "Besançon, France",
-      description: "Mode of Transport and Historical Road Network Growth Relationship, with Claire Lagesse.",
-      year: "February 2023-July 2023",
-    },
-    {
-      title: "Intern",
-      company: "ISI Foundation",
-      location: "Turin, Italy",
-      description: "Nature and Bicycle Network Relationship, with Rossano Schifanella.",
-      year: "October 2022-February 2023",
-    },
-    {
-      title: "Intern",
-      company: "IT University of Copenhagen",
-      location: "Copenhagen, Denmark",
-      description: "Prioritizing Links in Bicycle Network Plans With Subtractive Network Optimization, with Michael Szell.",
-      year: "February 2022-July 2022",
-    },
-    {
-      title: "Intern",
-      company: "LIPHY Laboratory for Interdisciplinary Physics",
-      location: "Grenoble, France",
-      description: "Collective Motion of Schooling Fish in a Complex Environment, with Aurélie Dupont and Philippe Peyla.",
-      year: "May 2021-July 2021",
-    },
-    {
-      title: "Intern",
-      company: "Ecole Normale Supérieure de Lyon",
-      location: "Lyon, France",
-      description: "Deep Learning for Time Series Predictions for FECG Monitoring, with Pierre Borgnat and Patrice Abry.",
-      year: "June 2020-July 2022",
-    },
-    {
-      title: "Intern",
-      company: "IM2NP",
-      location: "Marseille, France",
-      description: "Study of Optically Excited Magnetic States of ZnO by EPR, with Adrien Savoyant.",
-      year: "June 2019-July 2019",
-    },
-  ],
-  publications: [
-    {
-      title: "Long-Standing Optically Excited Magnetic States in ZnO Nanoparticles.",
-      authors: "Savoyant, A., Sebastiao, C., and Margeat, O.",
-      journal: "Physica Status Solidi RRL",
-      link: "https://doi.org/10.1002/pssr.202000176",
-      time: "2020",
-    },
-	    
-  ],
-  // Add more sections as needed
-};
+export const education = [
+	{
+		degree: "ENS Lyon Diploma",
+		school: "Ecole Normale Supérieure de Lyon",
+		location: "Lyon, France",
+		time: "2019-2023",
+	},
+	{
+		degree: "Master degree in Complex Systems, Physics",
+		school: "Ecole Normale Supérieure de Lyon",
+		location: "Lyon, France",
+		time: "2020-2022",
+	},
+	{
+		degree: "Bachelor degree in Physics",
+		school: "Claude Bernard University Lyon 1",
+		location: "Lyon, France",
+		time: "2017-2020",
+	},
+];
+
+export const experience = [
+	{
+		title: "PhD Student",
+		company: "IT University of Copenhagen",
+		location: "Copenhagen, Denmark",
+		description: "Optimizing urban sustainable mobility networks, part of the Just Streets EU Horizon Program. Supervised by Michael Szell and Luca Maria Aiello.",
+		year: "April 2024-Present",
+	},
+	{
+		title: "Research Assistant",
+		company: "IT University of Copenhagen",
+		location: "Copenhagen, Denmark",
+		year: "January 2024-April 2024",
+	},
+	{
+		title: "Intern",
+		company: "Université de Franche-Comté",
+		location: "Besançon, France",
+		description: "Mode of Transport and Historical Road Network Growth Relationship, with Claire Lagesse.",
+		year: "February 2023-July 2023",
+	},
+	{
+		title: "Intern",
+		company: "ISI Foundation",
+		location: "Turin, Italy",
+		description: "Nature and Bicycle Network Relationship, with Rossano Schifanella.",
+		year: "October 2022-February 2023",
+	},
+	{
+		title: "Intern",
+		company: "IT University of Copenhagen",
+		location: "Copenhagen, Denmark",
+		description: "Prioritizing Links in Bicycle Network Plans With Subtractive Network Optimization, with Michael Szell.",
+		year: "February 2022-July 2022",
+	},
+	{
+		title: "Intern",
+		company: "LIPHY Laboratory for Interdisciplinary Physics",
+		location: "Grenoble, France",
+		description: "Collective Motion of Schooling Fish in a Complex Environment, with Aurélie Dupont and Philippe Peyla.",
+		year: "May 2021-July 2021",
+	},
+	{
+		title: "Intern",
+		company: "Ecole Normale Supérieure de Lyon",
+		location: "Lyon, France",
+		description: "Deep Learning for Time Series Predictions for FECG Monitoring, with Pierre Borgnat and Patrice Abry.",
+		year: "June 2020-July 2022",
+	},
+	{
+		title: "Intern",
+		company: "IM2NP",
+		location: "Marseille, France",
+		description: "Study of Optically Excited Magnetic States of ZnO by EPR, with Adrien Savoyant.",
+		year: "June 2019-July 2019",
+	},
+];
+
+export const publications = [
+	{
+		title: "Long-Standing Optically Excited Magnetic States in ZnO Nanoparticles.",
+		authors: "Savoyant, A., Sebastiao, C., and Margeat, O.",
+		journal: "Physica Status Solidi RRL",
+		link: "https://doi.org/10.1002/pssr.202000176",
+		time: "2020",
+	},
+];
 
 export const skills = [
 	{
-		title: '',
-		description: '',
+		title: 'Programming skills',
+		description: 'Proficient in Python, especially with networks and geospatial data. Used before (by decreasing order of proficiency): PostgreSQL, R, Matlab.',
 	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
 ];
